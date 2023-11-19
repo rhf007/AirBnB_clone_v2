1bba47992907c782560dca37cb50907c8acf69e3
rhf007 <rahofahazem512@gmail.com>
2023-11-19 00:19:16 -0800

    lets revert back for a sec

--- models/place.py
+++ models/place.py
@@ -16,7 +16,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-    id = ""
-    def __init__(self, *args, **kwargs):
-        """init to inherit id and stuff"""
-        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -6,8 +6,3 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """ State class """
     name = ""
-    id = ""
-
-    def __init__(self, *args, **kwargs):
-        """init to inherit id and stuff"""
-        super().__init__(*args, **kwargs)
