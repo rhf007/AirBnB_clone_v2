cabecd9f6d01b82f03ea747beff7b2f2ae27f324
rhf007 <rahofahazem512@gmail.com>
2023-11-19 00:18:06 -0800

    lets revert back for a sec

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,20 +115,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-	# first we split args at every space
-        split_args = args.split(" ")
-        # Param syntax: <key name>=<value>
-        # so we need a dictionary or kwargs to save args
-        param_kwargs = {}
-        # llop on every parameter (key/value)
-        for i in range(1, len(aplit_args)):
-            k, v = split_args[i].split("=")
-            # if value has ""
-            if v == '"':
-                # remove
-                v.strip('"')
-                # replace _ with space
-		v.replace("_", " ")
+	
         if not args:
             print("** class name missing **")
             return
@@ -286,7 +273,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -294,10 +281,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
--- models/place.py
+++ models/place.py
@@ -16,3 +16,7 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+    id = ""
+    def __init__(self, *args, **kwargs):
+        """init to inherit id and stuff"""
+        super().__init__(*args, **kwargs)
