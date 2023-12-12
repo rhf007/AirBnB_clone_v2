ed41f37ebe5818f01e78f4c056db217910951e9e
rhf007 <rahofahazem512@gmail.com>
2023-12-12 15:16:27 -0800

    first things first

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,34 +1,33 @@
 #!/usr/bin/env bash
-# Prepare your web servers
+# Prepare your web servers, lets see
 apt-get update
 apt-get install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
-echo "THIS IS SOME SIMPLE TEST TEXT" > /data/web_static/releases/test/index.html
+echo "Hello World!" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chgrp -R ubuntu /data/
 chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 echo "server {
-    listen 80;
+    listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
-    root   /etc/nginx/html;
-    index  index.html;
+    root   /var/www/html;
+    index  index.html index.htm;
 
     location /hbnb_static {
-        alias /data/web_static/current/;
-	index index.html;
+        alias /data/web_static/current;
+        index index.html index.htm;
     }
 
     location /redirect_me {
-        return 301 https://www.instagram.com/rahofahazem/;
+        return 301 http://cuberule.com/;
     }
 
     error_page 404 /404.html;
-    location = /404.html{
-	    root /etc/nginx/html;
-	    internal;
+    location /404 {
+      root /var/www/html;
+      internal;
     }
 }" > /etc/nginx/sites-available/default
 service nginx restart
