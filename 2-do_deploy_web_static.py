0920d7455dfc93307b399e0fcfef9b182deb3de5
rhf007 <rahofahazem512@gmail.com>
2023-12-11 16:03:27 -0800

    please

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""a Fabric script (based on the file 1-pack_web_static.py) that distributes
+"""Fabric script (based on the file 1-pack_web_static.py) that distributes
 an archive to your web servers, using the function do_deploy"""
 
 
