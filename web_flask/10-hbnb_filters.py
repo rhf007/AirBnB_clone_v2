6396a023f74a2beab264a3f6b2307ab2750b3978
rhf007 <rahofahazem512@gmail.com>
2024-01-01 21:42:12 +0200

    certainly not passing pycodestyle but im too tired

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""HBNB"""
+
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_meth(e):
+    """teardown method to close SQLALCHEMY sessions"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
