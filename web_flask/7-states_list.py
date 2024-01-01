111be6465644dedfacd46410b458a4bf8b073f03
rhf007 <rahofahazem512@gmail.com>
2024-01-01 20:04:06 +0200

    jhjh

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""HBNB"""
+
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+@app.route('/states_list', strict_slashes=False)
+def states_list(n):
+    """doing operations on states"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+@app.teardown_appcontext
+def teardown_meth(e):
+    """teardown method to close SQLALCHEMY sessions"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
